ec047f5315b9f4c68dd2052a6db0965494590739
Jaugeni <jaugeni@gmail.com>
2020-03-04 11:47:44 -0800

    spacing

--- TopITunesAlbums/TopITunesAlbums/CustomViews/Button/FilledButton.swift
+++ TopITunesAlbums/TopITunesAlbums/CustomViews/Button/FilledButton.swift
@@ -15,18 +15,15 @@ class FilledButton: UIButton {
         configure()
     }
     
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
     
     convenience init(backgroundColor: UIColor, title: String) {
         self.init(frame: .zero)
         self.backgroundColor = backgroundColor
         self.setTitle(title, for: .normal)
     }
-    
     
     private func configure() {
         layer.cornerRadius  = 10
